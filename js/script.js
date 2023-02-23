01769c1ea823a6be29cd0132dd11dca9076d27d9
Tukei-david <dtukei831@gmail.com>
2023-02-23 12:21:35 +0300

    Implemeting the structure of script file using the self invoking function

---
+++ js/script.js
@@ -0,0 +1,35 @@
+console.log("Working!");
+
+
+
+
+/**
+ * Will be used to controll Employee object.
+ * Delete, create, update and claculate employee salary
+ */
+const EmployeeController = ( function () {
+
+})();
+
+
+
+
+
+/**
+ * Closure function will be used to read data from user interface.
+ * Updata data and display data in UI
+ */
+const UIController = ( function () {
+
+})();
+
+
+
+
+/**
+ * Used to control the flow of the app.
+ * Connect the EmployeeController and UIController
+ */
+const AppController = ( function (employCtrl, UIctrl) {
+
+})(EmployeeController, UIController);
