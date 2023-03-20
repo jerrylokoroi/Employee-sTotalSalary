f16ba674e9108166731f25ba5a62ba12fb72f421
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-03-20 11:25:23 +0000

    Fixed totalSalary function in the final.js file

--- js/main.js
+++ js/main.js
@@ -1,52 +1,43 @@
 
-var row = 1;
+const row = 1;
+const employees = [];
 
-var entry = document.getElementById("entry");
-entry.addEventListener("click", displayDetails);
+const addButton = document.getElementById("btn-add");
+addButton.addEventListener("click", displayDetails);
 
 
+function displayDetails(Id) {
 
-function displayDetails() {
 
-     let employees = [];
+     let firstName = getElementById("firstName");
+     let lastName = getElementById("lastName");
+     let age = getElementById("age");
+     let salary = getElementById("salary");
+     let zip = getElementById("zip");
+     let city = getElementById("city");
+     let street = getElementById("street");
+     let country = getElementById("country");
+     let pobox = getElementById("pobox");
 
-     var fname = document.getElementById("fname").value;
-     var lname = document.getElementById("lname").value;
-     var age = document.getElementById("age").value;
-     var salary = document.getElementById("salary").value;
-     var zip = document.getElementById("zip").value;
-     var city = document.getElementById("city").value;
-     var street = document.getElementById("street").value;
-     var country = document.getElementById("country").value;
-     var pobox = document.getElementById("pobox").value;
+     
 
-     let employee = {fname, lname, age, salary, zip, city, street, country};
-     console.log(employee);
+     
+     const display = document.getElementById("display");
 
-     employees.push(employee);
-     console.log(employees);
+     const newRow = display.insertRow(row);
 
-     if (!fname || !lname || !age || !salary || !zip || !city || !street || !country || !pobox) {
-          alert("Please fill in all the boxes");
-          return;
-     }
+     let cell1 = newRow.insertCell(0);
+     let cell2 = newRow.insertCell(1);
+     let cell3 = newRow.insertCell(2);
+     let cell4 = newRow.insertCell(3);
+     let cell5 = newRow.insertCell(4);
+     let cell6 = newRow.insertCell(5);
+     let cell7 = newRow.insertCell(6);
+     let cell8 = newRow.insertCell(7);
+     let cell9 = newRow.insertCell(8);
 
-     var display = document.getElementById("display");
-
-     var newRow = display.insertRow(row);
-
-     var cell1 = newRow.insertCell(0);
-     var cell2 = newRow.insertCell(1);
-     var cell3 = newRow.insertCell(2);
-     var cell4 = newRow.insertCell(3);
-     var cell5 = newRow.insertCell(4);
-     var cell6 = newRow.insertCell(5);
-     var cell7 = newRow.insertCell(6);
-     var cell8 = newRow.insertCell(7);
-     var cell9 = newRow.insertCell(8);
-
-     cell1.innerHTML = fname;
-     cell2.innerHTML = lname;
+     cell1.innerHTML = firstName;
+     cell2.innerHTML = lastName;
      cell3.innerHTML = age;
      cell4.innerHTML = salary;
      cell5.innerHTML = zip;
@@ -58,13 +49,88 @@ function displayDetails() {
      totalSalary()
 
      row++;
+
+     if (!fname || !lname || !age || !salary || !zip || !city || !street || !country || !pobox) {
+          alert("Please fill in all the boxes");
+          return;
+     }
+
+
 }
+
+function getElementById(id){
+     return document.getElementById(id).value;
+}
+
+// function displayDetails() {
+
+
+
+//      var fname = document.getElementById("firstName").value;
+//      var lname = document.getElementById("lastName").value;
+//      var age = document.getElementById("age").value;
+//      var salary = document.getElementById("salary").value;
+//      var zip = document.getElementById("zip").value;
+//      var city = document.getElementById("city").value;
+//      var street = document.getElementById("street").value;
+//      var country = document.getElementById("country").value;
+//      var pobox = document.getElementById("pobox").value;
+
+//      // let employee = {fname, lname, age, salary, zip, city, street, country};
+//      // console.log(employee);
+
+//      // employees.push(employee);
+//      // console.log(employees);
+
+//      let employee = {
+//           First:firstName,
+//           LastName:lastName,
+//           Salary:salary
+//      };
+//      console.log(employee);
+
+//      employees.push(employee);
+//      console.log(employees);
+
+//      if (!fname || !lname || !age || !salary || !zip || !city || !street || !country || !pobox) {
+//           alert("Please fill in all the boxes");
+//           return;
+//      }
+
+//      var display = document.getElementById("display");
+
+//      var newRow = display.insertRow(row);
+
+//      var cell1 = newRow.insertCell(0);
+//      var cell2 = newRow.insertCell(1);
+//      var cell3 = newRow.insertCell(2);
+//      var cell4 = newRow.insertCell(3);
+//      var cell5 = newRow.insertCell(4);
+//      var cell6 = newRow.insertCell(5);
+//      var cell7 = newRow.insertCell(6);
+//      var cell8 = newRow.insertCell(7);
+//      var cell9 = newRow.insertCell(8);
+
+//      cell1.innerHTML = fname;
+//      cell2.innerHTML = lname;
+//      cell3.innerHTML = age;
+//      cell4.innerHTML = salary;
+//      cell5.innerHTML = zip;
+//      cell6.innerHTML = city;
+//      cell7.innerHTML = street;
+//      cell8.innerHTML = country;
+//      cell9.innerHTML = pobox;
+
+//      totalSalary()
+
+     
+// }
 
 
 function totalSalary(){
-     var table = document.getElementById("display");
-     var salary = 0
-     for(var i = 1; i<table.rows.length; i++){
+     let table = document.getElementById("display");
+     let salary = 0
+     for(let i = 1; i<table.rows.length; i++){
           salary+=Number(table.rows[i].cells[3].innerText)
      }
      const totalInput = document.getElementById("totalSalary")
